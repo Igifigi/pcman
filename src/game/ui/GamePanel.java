40af6614c5e60bddda8c34df24663be3f8438ab2
Ignacy Świderski <ignacyswiderski@wp.pl>
2025-10-09 12:07:32 +0200

    Implement recaling when resolution changes

--- src/game/ui/GamePanel.java
+++ src/game/ui/GamePanel.java
@@ -1,13 +1,38 @@
 package game.ui;
 
 import java.awt.Color;
-
+import java.awt.Graphics;
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel {
 
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, getWidth(), getHeight());
+
+        int panelWidth = getWidth();
+        int panelHeight = getHeight();
+
+        int rectWidth = 224;
+        int rectHeight = 248;
+
+        if (panelWidth >= rectWidth * 2 && panelHeight >= rectHeight * 2) {
+            rectWidth *= 2;
+            rectHeight *= 2;
+        }
+
+        int x = (panelWidth - rectWidth) / 2;
+        int y = (panelHeight - rectHeight) / 2;
+
+        g.setColor(Color.BLUE);
+        g.fillRect(x, y, rectWidth, rectHeight);
+    }
+
     public GamePanel() {
         this.setBackground(Color.BLACK);
     }
-    
+
 }
