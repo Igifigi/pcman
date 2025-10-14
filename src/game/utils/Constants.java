9d8f3526dedc77936a6e29319d28effae9d69e07
TheWiseArdoni <tymon.mindstorms@gmail.com>
2025-10-14 15:43:32 +0200

    create Sprite class

--- src/game/utils/Constants.java
+++ src/game/utils/Constants.java
@@ -2,4 +2,7 @@ package game.utils;
 
 public final class Constants {
     public static final Size DEFAULT_WINDOW_SIZE = new Size(1000, 800);
+
+    public static final int TILE_SIZE = 32;
+    public static final String SPRITE_SHEET_FILE = "src/resources/textures/sprites.png";
 }
