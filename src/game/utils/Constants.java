833bfcc74e984a22ab5f26030a691a5e19d4d6f9
Ignacy Świderski <ignacyswiderski@wp.pl>
2025-10-14 20:42:00 +0200

    Refactor Size class as Tuple

--- src/game/utils/Constants.java
+++ src/game/utils/Constants.java
@@ -1,5 +1,5 @@
 package game.utils;
 
 public final class Constants {
-    public static final Size DEFAULT_WINDOW_SIZE = new Size(1000, 800);
+    public static final Tuple DEFAULT_WINDOW_SIZE = new Tuple(1000, 800);
 }
