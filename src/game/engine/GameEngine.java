6bf790f7ab12df912bc81298f8c7a89c7a577437
Ignacy Świderski <ignacyswiderski@wp.pl>
2025-10-14 21:03:14 +0200

    Feature 9: collision detection with walls

--- src/game/engine/GameEngine.java
+++ src/game/engine/GameEngine.java
@@ -1,7 +1,10 @@
 package game.engine;
 
+import game.entities.Player;
 import game.ui.GameFrame;
 import game.ui.GamePanel;
+import game.utils.Constants;
+import game.utils.KeyboardManager;
 
 public class GameEngine implements Runnable {
     private static final int FPS = Constants.FPS;
@@ -11,11 +14,19 @@ public class GameEngine implements Runnable {
     private Thread thread;
     private boolean running = false;
     private GamePanel panel;
+    private Player player;
+    private int tick = 0;
 
     public GameEngine(GameFrame frame) {
         panel = new GamePanel();
         frame.add(panel);
         frame.setVisible(true);
+
+        player = Player.getInstance();
+
+        panel.addKeyListener(new KeyboardManager());
+        panel.setFocusable(true);
+        panel.requestFocusInWindow();
     }
 
     public void start() {
@@ -27,7 +38,12 @@ public class GameEngine implements Runnable {
     @Override
     public void run() {
         while (running) {
+            if (tick % TPS == 0) {
+                player.updateLocation();
+            }
+            tick++;
             panel.repaint();
+
             try {
                 Thread.sleep(refreshRate);
             } catch (InterruptedException e) {
