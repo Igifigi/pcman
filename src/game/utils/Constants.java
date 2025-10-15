8fc7187dbb07eaabeeb2e1a6b302641a6b53cb3e
Ignacy Świderski <ignacyswiderski@wp.pl>
2025-10-15 21:23:20 +0200

    Update default window size

--- src/game/utils/Constants.java
+++ src/game/utils/Constants.java
@@ -1,7 +1,7 @@
 package game.utils;
 
 public final class Constants {
-    public static final Tuple DEFAULT_WINDOW_SIZE = new Tuple(1000, 800);
+    public static final Tuple DEFAULT_WINDOW_SIZE = new Tuple(1500, 1090);
     public static final int FPS = 60;
     public static final int TPS = (int) (60 / 10);
 
