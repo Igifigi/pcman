6f064425313cf5c429bbe1be35bf1a00fab2d0f1
Ignacy Świderski <ignacyswiderski@wp.pl>
2025-10-26 20:52:27 +0100

    Add run.bat script for building and running the project on Windows

---
+++ run.bat
@@ -0,0 +1,19 @@
+﻿@echo off
+setlocal enabledelayedexpansion
+set "SRC_DIR=src"
+set "OUT_DIR=tmp"
+
+if not exist "%OUT_DIR%" mkdir "%OUT_DIR%"
+
+
+dir /s /b "%SRC_DIR%\*.java" > "%OUT_DIR%\sources.txt"
+
+javac -d "%OUT_DIR%" @"%OUT_DIR%\sources.txt"
+
+del "%OUT_DIR%\sources.txt"
+
+if exist "%SRC_DIR%\resources\" (
+    xcopy "%SRC_DIR%\resources\*" "%OUT_DIR%\" /E /Y >nul 2>&1
+)
+
+java.exe -cp "%OUT_DIR%" game.Main
